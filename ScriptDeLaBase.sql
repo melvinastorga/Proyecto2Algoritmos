ba17cc64f4c5e35ba2b17dee286a235b8ed9ddb8
Andres Coto <andres.coto3539@gmail.com>
2018-06-18 08:58:44 -0600

    little bit change

--- ScriptDeLaBase.sql
+++ ScriptDeLaBase.sql
@@ -1,5 +1,7 @@
 create database Proyecto_2_Algoritmos
 use Proyecto_2_Algoritmos
+use master
+
 
 create table categoria(
 id int,
@@ -10,14 +12,14 @@ descripcion varchar(250)
 create table lote(
 id int,
 codigoLote varchar(100),
-fechaEmpacado date,
-fechaVencimiento date
+fechaEmpacado varchar(100),
+fechaVencimiento varchar(100)
 );
 
 create table unidadTransporte(
 id int,
 placa varchar(20),
-capacidad int,
+capacidad varchar(20),
 urlFotografia varchar(500)
 );
 
@@ -33,7 +35,7 @@ urlFotografia varchar(500)
 create table usuario(
 id int,
 nombre varchar(50),
-rol varchar(20),
+rol bit,
 usuario varchar(50),
 pass varchar(500),
 );
@@ -49,7 +51,7 @@ idOperador int
 
 create table productoMayorista(
 id int,
-idOrdenDistribucion int,
+idOrden int,
 nombre varchar(30),
 unidadMedida varchar(30),
 valorUnidad int,
@@ -60,5 +62,23 @@ idCategoria int,
 precioTotal bigint,
 urlFotografia varchar(500)
 );
+insert ordenDistribucion values(1,1,1,1,1,1)
+insert unidadTransporte values(1,'','','','')
+insert categoria values(2,'hola','adios')
+insert lote values(1,'','23-03-1995','23-02-1995')
+select * from categoria
+select * from lote
+select * from unidadTransporte
+select * from bodega
+select * from ordenDistribucion
+select * from usuario
+select * from productoMayorista where idOrden = '1'
 
+delete from categoria
+delete from lote
+delete from unidadTransporte
+delete from bodega
+delete from ordenDistribucion
+delete from usuario
+delete from productoMayorista
 
