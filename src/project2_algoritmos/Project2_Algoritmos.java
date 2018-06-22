b4052b333d4a49f3b905574d8637bbcf7795048d
melvinastorga <melvin.astorga@gmail.com>
2018-06-21 23:24:42 -0600

    pepe

--- src/project2_algoritmos/Project2_Algoritmos.java
+++ src/project2_algoritmos/Project2_Algoritmos.java
@@ -1,11 +1,14 @@
 
 package project2_algoritmos;
 
+import Interface.LoginPanel;
+
 
 public class Project2_Algoritmos {
 
     public static void main (String[] args){
- 
+        Interface.LoginPanel login = new LoginPanel();
+ login.show();
     }
 
 }
