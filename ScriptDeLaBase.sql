fff617f03f81f8011e24815871933a6400bf8694
Andres Coto <andres.coto3539@gmail.com>
2018-06-22 03:44:02 -0600

    progress bar funcionando agrega productos

--- ScriptDeLaBase.sql
+++ ScriptDeLaBase.sql
@@ -38,7 +38,8 @@ rol bit,
 usuario varchar(50),
 pass varchar(500),
 );
-
+insert usuario values(1,'Andres',0,'operador','operador')
+insert usuario values(2,'Melvin',1,'admin','admin')
 create table ordenDistribucion(
 id int,
 idBodegaProcedencia int,
@@ -73,13 +74,21 @@ idCategoria int,
 precioTotal bigint,
 urlFotografia varchar(500)
 );
-insert productoMayorista values(1,1,'','',1,1,'',1,1,1,'')
+insert productoMayorista values(1,'Arroz','kilogramos',2500,100,'arroz chino',1,1,25000,'imagen')
+insert productoMayorista values(2,'Frijoles','kilogramos',1500,70,'frijol chino',1,2,20000,'imagen')
+insert productoMayorista values(3,'Azucar','kilogramos',700,50,'azucar chino',1,3,5000,'imagen')
+insert productoMayorista values(4,'Cafe','kilogramos',750,25,'cafe chino',1,4,2000,'imagen')
+insert productoMayorista values(5,'Cafe pesado','kilogramos',750,500,'cafe chino',1,4,2000,'imagen')
 insert ordenDistribucion values(1,1,1,1,1,1)
 insert unidadTransporte values(1,'','','','')
 insert categoria values(2,'hola','adios')
 insert lote values(1,'','23-03-1995','23-02-1995')
 insert categoria values(1,'','','',1,'')
-insert bodega values(1,'','','',1,'')
+insert bodega values(1,'Alcenes Iman','X','Y',235,'imagen')
+insert bodega values(2,'Almacenes Jiron','X','Y',235,'imagen')
+insert bodega values(3,'La bodega','X','Y',235,'imagen')
+insert bodega values(4,'Guarida No somos narcos','X','Y',235,'imagen')
+insert bodega values(5,'New York Inc.','X','Y',235,'imagen')
 select * from categoria
 select * from lote
 select * from unidadTransporte
